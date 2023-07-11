caa2fb18a5d3abad4eaf98e31bd3b5c94d5b69ec
Riddhi Kumari Singh <riddhisi.it@gmail.com>
2023-07-11 07:04:32 +0530

    Landing Page

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function LandingPage() {
+  return (
+    <div>
+      <h1>Welcome to the Landing Page</h1>
+      {/* Add more content for your landing page */}
+    </div>
+  );
+}
+
+export default LandingPage;
